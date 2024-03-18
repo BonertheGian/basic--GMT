7642b5a50a17c11df8309218cded17065db426df
AiraJoy31 <eling011259@gmail.com>
2024-03-18 10:56:25 +0800

    pogi si gian

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ const hNav = document.querySelector(".header__nav")
 burgir.addEventListener("click", () =>{
     burgir.classList.toggle("open");
     hNav.classList.toggle("open");
+
 })
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('.header');
+  
+    window.addEventListener('scroll', function () {
+      if (window.scrollY > 100) {
+        header.classList.add('header-scrolled');
+      } else {
+        header.classList.remove('header-scrolled');
+      }
+    });
+
+});
